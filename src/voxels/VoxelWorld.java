ea576875bd472a976a9418fd7ce4e01b2d73ae11
melsov <melsovereign@yahoo.com>
2014-08-10 19:34:34 -0400

    first commit

---
+++ src/voxels/VoxelWorld.java
@@ -0,0 +1,75 @@
+package voxels;
+
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.asset.AssetManager;
+import com.jme3.material.Material;
+import com.jme3.material.RenderState;
+import com.jme3.math.ColorRGBA;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Mesh;
+import com.jme3.scene.shape.Cylinder;
+
+/**
+ * Created by didyouloseyourdog on 8/10/14.
+ */
+public class VoxelWorld extends SimpleApplication
+{
+    MaterialLibrarian materialLibrarian;
+
+    @Override
+    public void simpleUpdate(float secondsPerFrame) {
+
+    }
+
+    @Override
+    public void simpleInitApp() {
+        materialLibrarian = new MaterialLibrarian(assetManager);
+        setUpTheCam();
+        makeADemoMeshAndAdditToTheRootNode();
+
+    }
+
+    private void makeADemoMeshAndAdditToTheRootNode() {
+        Mesh m = new Cylinder(12,24,5,11);
+        Geometry g = new Geometry("demo geom", m);
+        g.setMaterial(materialLibrarian.getBlockMaterial());
+        rootNode.attachChild(g);
+    }
+    private void setUpTheCam() {
+        flyCam.setEnabled(true);
+        flyCam.setMoveSpeed(30);
+    }
+
+    /*******************************
+     * Program starts here... ******
+     *******************************/
+    public static void main(String[] args)
+    {
+        VoxelWorld app = new VoxelWorld();
+        app.start(); // start the game
+    }
+
+    public class MaterialLibrarian
+    {
+        private Material blockMaterial;
+        private AssetManager _assetManager;
+
+        public MaterialLibrarian(AssetManager assetManager_) {
+            _assetManager = assetManager_;
+        }
+
+        public Material getBlockMaterial() {
+            if (blockMaterial == null) {
+                Material wireMaterial = new Material(assetManager, "/Common/MatDefs/Misc/Unshaded.j3md");
+                wireMaterial.setColor("Color", ColorRGBA.Green);
+                wireMaterial.getAdditionalRenderState().setWireframe(true);
+                wireMaterial.getAdditionalRenderState().setFaceCullMode(RenderState.FaceCullMode.Off);
+                blockMaterial = wireMaterial;
+            }
+            return blockMaterial;
+        }
+    }
+
+
+}
